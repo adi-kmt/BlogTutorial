92b7b5f3813c0848a16b840261574b936639d4d8
adithyakamath <adithya.kamath@mutualmobile.com>
2022-10-11 19:57:08 +0530

    Excpetion handled

---
+++ src/main/java/com/adithya/BlogTutorial/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.adithya.BlogTutorial.exceptions;
+
+import com.adithya.BlogTutorial.exceptions.models.ResourceNotFoundException;
+import com.adithya.BlogTutorial.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException exception){
+        String message = exception.getMessage();
+        ApiResponse response = new ApiResponse(message, false);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
