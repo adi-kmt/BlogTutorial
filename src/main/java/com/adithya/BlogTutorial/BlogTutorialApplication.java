40ef0e35526849e379e905bd1fe0cb7e35cd46f2
adithyakamath <adithya.kamath@mutualmobile.com>
2022-10-11 18:17:51 +0530

    Basic structure

---
+++ src/main/java/com/adithya/BlogTutorial/BlogTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.adithya.BlogTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adithya/BlogTutorial/BlogTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adithya.BlogTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
